798e63122caa91af634d880ca32dd86bcddff8e9
Grigori Fursin <grigori.fursin@cTuning.org>
2015-06-14 20:29:48 +0200

    adding first version of interactive reports

--- module/report/module.py
+++ module/report/module.py
@@ -29,3 +29,150 @@ def init(i):
 
     """
     return {'return':0}
+
+##############################################################################
+# viewing entry as html
+
+def html_viewer(i):
+    """
+    Input:  {
+              data_uoa
+
+              url_base
+              url_pull
+
+              url_pull_tmp
+              tmp_data_uoa
+
+              url_wiki
+
+              html_share
+
+              form_name     - current form name
+
+              (all_params)
+            }
+
+    Output: {
+              return       - return code =  0, if successful
+                                         >  0, if error
+              (error)      - error text if return > 0
+            }
+
+    """
+    import os
+
+    h=''
+    raw='no'
+    top='yes'
+
+    duoa=i['data_uoa']
+    burl=i['url_base']
+    purl=i['url_pull']
+    wurl=i.get('url_wiki','')
+
+    tpurl=i['url_pull_tmp']
+    tpuoa=i['tmp_data_uoa']
+
+    ap=i.get('all_params',{})
+
+    ruoa=ap.get('ck_top_repo','')
+    muoa=ap.get('ck_top_module','')
+
+    cparams=ap.get('graph_params','') # current graph params
+
+    hshare=i.get('html_share','')
+
+    form_name=i['form_name']
+    form_submit='document.'+form_name+'.submit();'
+
+    if duoa!='':
+       # Load entry
+       rx=ck.access({'action':'load',
+                     'module_uoa':work['self_module_uid'],
+                     'data_uoa':duoa})
+       if rx['return']>0: return rx
+
+       pp=rx['path']
+
+       dd=rx['dict']
+       duid=rx['data_uid']
+
+       if dd.get('live','')!='yes':
+          raw='yes'
+       else:
+          title=dd.get('title','')
+          authors=dd.get('authors',[])
+          affs=dd.get('affiliations',{})
+          cauthor=dd.get('cor_author_email','')
+
+          h+='<div id="ck_entries">\n'
+       
+          h+='<center><span id="ck_article_title">'+title+'</span><br>'
+
+          if len(authors)!='':
+             h+='<div id="ck_entries_space4"></div>\n'
+             h+='<span id="ck_article_authors">\n'
+
+             x=''
+             for a in authors:
+                 name=a.get('name','')
+                 aff=a.get('affiliation','')
+                 url=a.get('url','')
+
+                 if url!='': name='<a href="'+url+'">'+name+'</a>'
+
+                 if x!='': x+=', '
+                 x+=name+'&nbsp;<sup>'+aff+'</sup>'
+
+             h+=x+'<br>\n'
+             h+='</span>\n'
+
+          if len(affs)>0:
+             h+='<div id="ck_entries_space4"></div>\n'
+             h+='<span id="ck_article_affiliations">\n'
+
+             x=''
+             for a in sorted(affs, key=int):
+                 af=affs[str(a)]
+                 name=af.get('name','')
+
+                 if x!='': x+=',&nbsp;&nbsp;&nbsp;'
+                 x+='<sup>'+str(a)+'</sup>&nbsp;'+name
+
+             h+=x+'<br>\n'
+             h+='</span>\n'
+
+          h+='</center>\n'
+
+          if len(cauthor)>0:
+             h+='<div id="ck_entries_space4"></div>\n'
+             h+='<span id="ck_article_cauthor">\n'
+             h+='<i>Corresponding author: <a href="mailto:'+cauthor+'">'+cauthor+'</a></i>\n'
+             h+='</span>\n'
+
+          h+='<div id="ck_entries_space4"></div>\n'
+          h+='<div id="ck_entries_space4"></div>\n'
+          h+='<div id="ck_entries_space4"></div>\n'
+          h+='<div id="ck_entries_space4"></div>\n'
+
+          # Checking template
+          t=dd.get('template','')
+          if t!='':
+             h+='<span id="ck_article_text">\n'
+
+             px=os.path.join(pp,t)
+             if os.path.isfile(px):
+                rx=ck.load_text_file({'text_file':px})
+                if rx['return']>0: return rx
+                th=rx['string']
+
+
+
+             h+=th
+
+             h+='</span\n'
+
+          h+='</div>\n'
+
+    return {'return':0, 'raw':raw, 'show_top':top, 'html':h}
