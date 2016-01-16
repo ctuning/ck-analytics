848b557a5fd323393cccad990f08dfb0dbd479c5
Grigori Fursin <grigori.fursin@cTuning.org>
2016-01-16 02:05:11 +0100

    adding more frontier filter demos

---
+++ demo/pareto-frontier/filter_frontier_1d.bat
@@ -0,0 +1,1 @@
+ck filter math.frontier @frontier_1d.json out=json_file out_file=frontier_1d_out.json
---
+++ demo/pareto-frontier/filter_frontier_1da.bat
@@ -0,0 +1,1 @@
+ck filter math.frontier @frontier_1da.json out=json_file out_file=frontier_1da_out.json
