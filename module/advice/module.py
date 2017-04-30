0a184f69762151d45aeb14ccef1745ca35a4b839
Grigori Fursin <grigori.fursin@cTuning.org>
2017-04-30 12:17:59 +0200

    started improving collaborative AI API

--- module/advice/module.py
+++ module/advice/module.py
@@ -15,6 +15,18 @@ ck=None # Will be updated by CK (initialized CK kernel)
 
 # Local settings
 
+hextra='<i><center>\n'
+hextra+='This is a community-driven R&D: \n'
+hextra+=' [ <a href="http://cKnowledge.org/ai">collaborative AI optimization</a> ], '
+hextra+=' [ <a href="https://github.com/dividiti/ck-caffe">CK-Caffe</a> ], '
+hextra+=' [ <a href="https://github.com/ctuning/ck-tensorflow">CK-TensorFlow</a> ], '
+hextra+=' [ <a href="https://en.wikipedia.org/wiki/Collective_Knowledge_(software)">CK intro</a>, \n'
+hextra+='CK papers: <a href="https://www.researchgate.net/publication/304010295_Collective_Knowledge_Towards_RD_Sustainability">1</a> and \n'
+hextra+='<a href="https://arxiv.org/abs/1506.06256">2</a>; \n'
+hextra+='<a href="https://www.youtube.com/watch?v=Q94yWxXUMP0">YouTube intro</a> ] \n'
+hextra+='</center></i>\n'
+hextra+='<br>\n'
+
 ##############################################################################
 # Initialize module
 
@@ -31,3 +43,51 @@ def init(i):
 
     """
     return {'return':0}
+
+##############################################################################
+# access available CK-AI self-optimizing functions
+
+def show(i):
+    """
+    Input:  {
+            }
+
+    Output: {
+              return       - return code =  0, if successful
+                                         >  0, if error
+              (error)      - error text if return > 0
+            }
+
+    """
+
+    h=''
+    h+='<center>\n'
+    h+='\n\n<script language="JavaScript">function copyToClipboard (text) {window.prompt ("Copy to clipboard: Ctrl+C, Enter", text);}</script>\n\n' 
+
+#    h+='<h2>Aggregated results from Caffe crowd-benchmarking (time, accuracy, energy, cost, ...)</h2>\n'
+
+    h+=hextra
+
+    # Check host URL prefix and default module/action
+    rx=ck.access({'action':'form_url_prefix',
+                  'module_uoa':'wfe',
+                  'host':i.get('host',''), 
+                  'port':i.get('port',''), 
+                  'template':i.get('template','')})
+    if rx['return']>0: return rx
+    url0=rx['url']
+    template=rx['template']
+
+    url=url0
+    action=i.get('action','')
+    muoa=i.get('module_uoa','')
+
+    st=''
+
+    url+='action=index&module_uoa=wfe&native_action='+action+'&'+'native_module_uoa='+muoa
+    url1=url
+
+
+    h+=url
+
+    return {'return':0, 'html':h, 'style':st}
