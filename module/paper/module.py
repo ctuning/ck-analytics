bb8393d0446ed61397c0d04c1eeea528294c6d38
Grigori Fursin <Grigori.Fursin@cTuning.org>
2020-08-19 09:22:11 +0200

    added module:paper

---
+++ module/paper/module.py
@@ -0,0 +1,53 @@
+#
+# Collective Knowledge (CK wrapper for articles)
+#
+# See CK LICENSE.txt for licensing details
+# See CK COPYRIGHT.txt for copyright details
+#
+# Developer: Grigori Fursin, Grigori.Fursin@cTuning.org, http://fursin.net
+#
+
+cfg={}  # Will be updated by CK (meta description of this module)
+work={} # Will be updated by CK (temporal data)
+ck=None # Will be updated by CK (initialized CK kernel) 
+
+# Local settings
+
+##############################################################################
+# Initialize module
+
+def init(i):
+    """
+
+    Input:  {}
+
+    Output: {
+              return       - return code =  0, if successful
+                                         >  0, if error
+              (error)      - error text if return > 0
+            }
+
+    """
+    return {'return':0}
+
+##############################################################################
+# process article
+
+def preprocess(i):
+    """
+    Input:  {
+            }
+
+    Output: {
+              return       - return code =  0, if successful
+                                         >  0, if error
+              (error)      - error text if return > 0
+            }
+
+    """
+
+    # Redirect to dissemination.publication
+
+    i['module_uoa']=cfg['module_deps']['dissemination.publication']
+
+    return ck.access(i)
